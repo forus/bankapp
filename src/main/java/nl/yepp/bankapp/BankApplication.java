cae82c860017259e8b4aa5765cac406fd70d4b82
Ruslan Forostianov <ruslan@forostianov.com>
2022-10-10 06:38:25 +0200

    Enable basic authentication only

--- src/main/java/nl/yepp/bankapp/BankApplication.java
+++ src/main/java/nl/yepp/bankapp/BankApplication.java
@@ -2,8 +2,10 @@ package nl.yepp.bankapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SecurityConfiguration.class)
 public class BankApplication {
 
 	public static void main(String[] args) {
